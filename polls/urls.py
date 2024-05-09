2dbc2a252af6980f6a632aef1c63db9f2607c33f
wayne71 <wayne.hatter1971@gmail.com>
2024-05-09 18:29:14 -0500

    wire in the newly created views

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    # /polls/
+    path("", views.index, name="index"),
+    # /polls/1/
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote")
 ]
