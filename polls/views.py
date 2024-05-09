3b0e0cb4512344e7b350c10ef5fa9075aa713e55
wayne71 <wayne.hatter1971@gmail.com>
2024-05-09 18:54:56 -0500

    refactor index view to return render

--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,7 @@ def index(request):
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/indexx.html", context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
