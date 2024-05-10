ab89d1af10b1148d2a5c465f2db07d68ca1f6164
wayne71 <wayne.hatter1971@gmail.com>
2024-05-10 18:17:04 -0500

    set detail path in urlls.py anend detail view

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 from django.template import loader
+
+
 from .models import Question
 
 def index(request):
@@ -13,7 +15,8 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     respnse = "You're looking at the results of question %s."
