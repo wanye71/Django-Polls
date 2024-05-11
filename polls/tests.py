a8c05059e3c91d55850233bf1bb65675cdfa1725
wayne71 <wayne.hatter1971@gmail.com>
2024-05-11 10:01:35 -0500

    add QuestionDetailViewTests class to tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -89,4 +89,15 @@ class QuestionIndexViewTests(TestCase):
             [question2, question1],
         )
         
-    
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text="Future question", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+        
+    def test_past_question(self):
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
