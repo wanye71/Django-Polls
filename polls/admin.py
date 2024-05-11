57fad9edcfeb94d01cafad443e31b47c395d4ba6
wayne71 <wayne.hatter1971@gmail.com>
2024-05-11 11:50:52 -0500

    add fieldset in admin

--- polls/admin.py
+++ polls/admin.py
@@ -5,6 +5,10 @@ from .models import Question
 # admin.site.register(Question)
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]
+    # fields = ["pub_date", "question_text"]
+    fieldsets = [
+        ("Questions", {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]})
+    ]
 
 admin.site.register(Question, QuestionAdmin)
